fc07dd432886981bbd9ff8769c60adbf4fa60f48
Sam Brown <sambrown@Sams-MacBook-Pro.local>
2021-01-29 12:07:25 -0800

    Styling for setting page

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -5,10 +5,49 @@ import Styled from "styled-components"
 import {FaEdit} from "react-icons/fa"
 
 const StyledSettings = Styled.div`
-height: 90vh;
+/* height: 90vh; */
 background-color: rgba(255, 255, 255, 0.05);
 overflow-y: auto;
 color: white;
+text-align: left;
+width: 60%;
+margin: 0 auto;
+border-radius: 10px;
+padding: 0 4%;
+
+h3 {
+  text-align: center;
+  padding-top: 3%;
+}
+hr {
+  background-color: white;
+}
+input {
+  width: 100%;
+}
+input::placeholder {
+  color: #9b9b9b;
+}
+.form-group {
+  display: flex;
+  align-items: center;
+  .form-label {
+    width: 40%;
+    padding-left: 2%;
+    font-weight: bold;
+  }
+  .form-input {
+    width: 60%;
+  }
+}
+label {
+  margin: 0;
+}
+.button {
+  text-align: center;
+  margin: 1% 0 3% 0;
+ 
+}
 `
 
 const initFormValues = {
@@ -68,143 +107,201 @@ export default function Settings() {
       <>
       {isEditing ? (<StyledSettings>
             <h3>Edit Profile</h3>
+            <hr/>
             <Form>
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group className="form-group">
+          <div className="form-label"><label>
           Email:
-          <input
+            </label></div>
+
+          <div className="form-input">
+            <input
             className="featureless-input"
             type="email"
             onChange={handleOnchange}
             value={formValues.email}
             placeholder="Enter email"
             name="email"
-          />
+            />
+            </div>
         </Form.Group>
-        <Form.Group controlId="formBasicEmail">
+            <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label"><label>
           Username:
-          <input
+            </label></div>
+          <div className="form-input">
+            <input
             className="featureless-input"
             type="text"
             onChange={handleOnchange}
             value={formValues.username}
             placeholder="Username"
             name="username"
-          />
+            />
+            </div>
         </Form.Group>
-        <Form.Group controlId="formBasicPassword">
+            <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label"><label>
           Password:
-          <input
+            </label></div>
+          <div className="form-input">
+            <input
             className="featureless-input"
             type="password"
             onChange={handleOnchange}
             value={formValues.password}
             placeholder="Password"
             name="password"
-          />
+            />
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
+            <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label"><label>
           First Name:
-          <input
+            </label></div>
+          <div className="form-input">
+            <input
             className="featureless-input"
             type="text"
             onChange={handleOnchange}
             value={formValues.first_name}
             placeholder="First Name"
             name="first_name"
-          />
+            />
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
+            <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label"><label>
           Last Name:
-          <input
+            </label></div>
+          <div className="form-input">
+            <input
             className="featureless-input"
             type="text"
             onChange={handleOnchange}
             value={formValues.last_name}
             placeholder="Last Name"
             name="last_name"
-          />
+            />
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
+            <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label"><label>
           Street Address:
-          <input
+            </label></div>
+          <div className="form-input">
+            <input
             className="featureless-input"
             type="text"
             onChange={handleOnchange}
             value={formValues.street_address}
             placeholder="Street Address"
             name="street_address"
-          />
+            />
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
+            <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label"><label>
           City:
-          <input
+            </label></div>
+          <div className="form-input">
+            <input
             className="featureless-input"
             type="text"
             onChange={handleOnchange}
             value={formValues.city}
             placeholder="City"
             name="city"
-          />
+            />
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
+            <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label"><label>
           State:
-          <input
+            </label></div>
+          <div className="form-input">
+            <input
             className="featureless-input"
             type="text"
             onChange={handleOnchange}
             value={formValues.state}
             placeholder="State"
             name="state"
-          />
+            />
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
+            <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label"><label>
           Zip Code:
-          <input
+            </label></div>
+          <div className="form-input">
+            <input
             className="featureless-input"
             type="text"
             onChange={handleOnchange}
             value={formValues.zipcode}
             placeholder="Zip Code"
             name="zipcode"
-          />
+            />
+            </div>
         </Form.Group>
+            <hr/>
         {formValues.isGrowr > 0 ? 
         (
         <>
-					<Form.Group controlId="">
+					<Form.Group className="form-group">
+            <div className="form-label"><label>
             Hourly Rate:
-						<input
+              </label></div>
+						<div className="form-input">
+              <input
 						className="featureless-input"
 						type="text"
 						onChange={handleOnchange}
 						value={formValues.hourly_rate}
 						placeholder="Hourly Rate"
 						name="hourly_rate"
-						/> /hr
+						/> 
+            </div>
 					</Form.Group>
-					<Form.Group controlId="">
+            <hr/>
+					<Form.Group className="form-group">
+            <div className="form-label"><label>
             Maximum Range (miles):
-						<input
+              </label></div>
+						<div className="form-input">
+              <input
 							className="featureless-input"
 							type="text"
 							onChange={handleOnchange}
 							value={formValues.max_mile_range}
 							placeholder="Max Mile Range"
 							name="max_mile_range"
-						/>
-            miles
+              />
+              </div>
 					</Form.Group>
+              <hr/>
         </>
     		)    
     		:null}
+        <div className="button">
 
-        <button
-          type="submit"
-          className="clamped-text cta-button"
-          onClick={handleSubmit}
-        >
-          Update Profile
-        </button>
+          <button
+            type="submit"
+            className="clamped-text"
+            onClick={handleSubmit}
+            >
+            Update Profile
+          </button>
+        </div>
       </Form>
             
     </StyledSettings>
@@ -213,65 +310,148 @@ export default function Settings() {
     (
     <StyledSettings>
             <h3>Account Information</h3>
+            <hr/>
             <Form>
-        <Form.Group controlId="formBasicEmail">
-          Email:
-          <span>{formValues.email}</span>
-        </Form.Group>
+        <Form.Group className="form-group">
+          <div className="form-label">
+            <label>
+              Email:
+              </label>
+            </div>
+            <div className="form-input">
 
-        <Form.Group controlId="formBasicEmail">
-          Username:
+          <span>{formValues.email}</span>
+            </div>
+        </Form.Group>
+        <hr/>
+
+        <Form.Group className="form-group">
+          <div className="form-label">
+            <label>
+              Username:
+              </label>
+            </div>
+            <div className="form-input">
+
           <span>{formValues.username}</span>
+            </div>
         </Form.Group>
+        <hr/>
        
-        <Form.Group controlId="">
-          First Name:
+        <Form.Group className="form-group">
+          <div className="form-label">
+            <label>
+              First Name:
+              </label>
+            </div>
+            <div className="form-input">
+
           <span>{formValues.first_name}</span>
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
-          Last Name:
+        <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label">
+            <label>
+              Last Name:
+              </label>
+            </div>
+            <div className="form-input">
+
           <span>{formValues.last_name}</span>
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
-          Street Address:
+        <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label">
+            <label>
+              Street Address:
+              </label>
+            </div>
+            <div className="form-input">
+
           <span>{formValues.street_address}</span>
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
-          City:
+        <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label">
+            <label>
+              City:
+              </label>
+            </div>
+            <div className="form-input">
+
           <span>{formValues.city}</span>
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
-          State:
+        <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label">
+            <label>
+              State:
+              </label>
+            </div>
+            <div className="form-input">
+
           <span>{formValues.state}</span>
+            </div>
         </Form.Group>
-        <Form.Group controlId="">
-          Zip Code:
+        <hr/>
+        <Form.Group className="form-group">
+          <div className="form-label">
+            <label>
+              Zip Code:
+              </label>
+            </div>
+            <div className="form-input">
+
           <span>{formValues.zipcode}</span>
+            </div>
         </Form.Group>
+        <hr/>
         {formValues.isGrowr > 0 ? 
         (
         <>
-					<Form.Group controlId="">
-            Hourly Rate:
-						<span>{formValues.hourly_rate}</span> /hr
+					<Form.Group className="form-group">
+            <div className="form-label">
+              <label>
+                Hourly Rate:
+                </label>
+              </div>
+              <div className="form-input">
+
+						<span>{formValues.hourly_rate}</span> 
+              </div>
 					</Form.Group>
-					<Form.Group controlId="">
-            Maximum Range (miles):
+          <hr/>
+					<Form.Group className="form-group">
+            <div className="form-label">
+              <label>
+                Maximum Range (miles):
+                </label>
+              </div>
+              <div className="form-input">
+
 						<span>{formValues.max_mile_range}</span>
-					 miles
+              </div>
+					 
 					</Form.Group>
+          <hr/>
         </>
     		)    
     		:null}
+        <div className="button">
 
         <button
           type="submit"
-          className="clamped-text cta-button"
+          className="clamped-text"
           onClick={() => setIsEditing(!isEditing)}
-        >
+          >
           Edit Profile
           <FaEdit/>
         </button>
+          </div>
       </Form>
             
     </StyledSettings>)}
