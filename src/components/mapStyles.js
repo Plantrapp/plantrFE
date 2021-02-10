6c887e9ab89bba26bb3105fba38cad0716e26408
Sam Brown <sambrown@Sams-MacBook-Pro.local>
2021-02-10 10:50:22 -0800

    Edits map

--- src/components/mapStyles.js
+++ src/components/mapStyles.js
@@ -1,44 +1,148 @@
 export default [
   {
-    featureType: "road",
-    elementType: "geometry",
+    featureType: "landscape",
     stylers: [
       {
-        lightness: 100,
+        hue: "#FFA800",
       },
       {
-        visibility: "simplified",
+        saturation: 0,
+      },
+      {
+        lightness: 0,
+      },
+      {
+        gamma: 1,
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    stylers: [
+      {
+        hue: "#53FF00",
+      },
+      {
+        saturation: -73,
+      },
+      {
+        lightness: 40,
+      },
+      {
+        gamma: 1,
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    stylers: [
+      {
+        hue: "#FBFF00",
+      },
+      {
+        saturation: 0,
+      },
+      {
+        lightness: 0,
+      },
+      {
+        gamma: 1,
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    stylers: [
+      {
+        hue: "#00FFFD",
+      },
+      {
+        saturation: 0,
+      },
+      {
+        lightness: 30,
+      },
+      {
+        gamma: 1,
       },
     ],
   },
   {
     featureType: "water",
-    elementType: "geometry",
     stylers: [
       {
-        visibility: "on",
+        hue: "#00BFFF",
       },
       {
-        color: "#C6E2FF",
+        saturation: 6,
+      },
+      {
+        lightness: 8,
+      },
+      {
+        gamma: 1,
       },
     ],
   },
   {
     featureType: "poi",
-    elementType: "geometry.fill",
     stylers: [
       {
-        color: "#C5E3BF",
+        hue: "#679714",
       },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "geometry.fill",
-    stylers: [
       {
-        color: "#D1D1B8",
+        saturation: 33.4,
+      },
+      {
+        lightness: -25.4,
+      },
+      {
+        gamma: 1,
       },
     ],
   },
 ];
+// [
+//   {
+//     featureType: "road",
+//     elementType: "geometry",
+//     stylers: [
+//       {
+//         lightness: 100,
+//       },
+//       {
+//         visibility: "simplified",
+//       },
+//     ],
+//   },
+//   {
+//     featureType: "water",
+//     elementType: "geometry",
+//     stylers: [
+//       {
+//         visibility: "on",
+//       },
+//       {
+//         color: "#C6E2FF",
+//       },
+//     ],
+//   },
+//   {
+//     featureType: "poi",
+//     elementType: "geometry.fill",
+//     stylers: [
+//       {
+//         color: "#C5E3BF",
+//       },
+//     ],
+//   },
+//   {
+//     featureType: "road",
+//     elementType: "geometry.fill",
+//     stylers: [
+//       {
+//         color: "#D1D1B8",
+//       },
+//     ],
+//   },
+// ];
